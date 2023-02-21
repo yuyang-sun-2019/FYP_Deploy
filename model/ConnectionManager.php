071816f4937a40208e5005b0aaafe24270e90e5e
yuyang-sun-2019 <81420946+yuyang-sun-2019@users.noreply.github.com>
2023-02-21 19:07:23 +0800

    Update ConnectionManager.php

--- model/ConnectionManager.php
+++ model/ConnectionManager.php
@@ -21,12 +21,12 @@ class ConnectionManager { -->
     //     // Return connection object
     //     return $conn; -->
 <?php
-    $url = parse_url(getenv("mysql://b805ab9b5bfcbd:2b244a70@us-cdbr-east-06.cleardb.net/heroku_496d18e9fc14ec0?reconnect=true"));
+    // $url = parse_url(getenv("mysql://b805ab9b5bfcbd:2b244a70@us-cdbr-east-06.cleardb.net/heroku_496d18e9fc14ec0?reconnect=true"));
     
-    $server = $url["us-cdbr-east-06.cleardb.com"];
-    $username = $url["b805ab9b5bfcbd"];
-    $password = $url["2b244a70"];
-    $db = substr($url["heroku_496d18e9fc14ec0"], 1);
+    $server = "us-cdbr-east-06.cleardb.com";
+    $username = "b805ab9b5bfcbd";
+    $password = "2b244a70";
+    $db = "heroku_496d18e9fc14ec0";
     
     $conn = new mysqli($server, $username, $password, $db);
     ?>
