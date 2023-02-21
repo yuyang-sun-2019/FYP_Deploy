e588d14d69c9361e9f67185c06802e4520c8b8a0
yuyang-sun-2019 <81420946+yuyang-sun-2019@users.noreply.github.com>
2023-02-21 12:20:17 +0800

    Update create.sql

--- database/create.sql
+++ database/create.sql
@@ -1,7 +1,3 @@
-
-use postgres://qwzfvmxbujfhbs:01f956c480a7f0365fffbb3ff26a29e1b765779557f84a03aeb06a3b6f9be08a@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d1lqtpv46396rh 
-
-
 CREATE TABLE `user` (
  `email` varchar(50) NOT NULL,
  `hashed_password` varchar(256) ,
