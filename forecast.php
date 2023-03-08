bd20209e4be9b85921d70623a1146d3e15e286ab
yuyang-sun-2019 <81420946+yuyang-sun-2019@users.noreply.github.com>
2023-03-08 18:27:30 +0800

    Update forecast.php

--- forecast.php
+++ forecast.php
@@ -1,6 +1,21 @@
 <html>
-    <div>
+<head>
+        <style>
+            .geeks {
+                width:100%;
+                height:1000px;
+            }
+            img {
+                width:100%;
+                height:100%;
+                object-fit:cover;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="geeks">
     <img src="/images/sales_forecast_excel.png"/>
     </div>
+    </body>
 
 </html>
