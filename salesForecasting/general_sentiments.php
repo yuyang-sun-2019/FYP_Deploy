adadd3ffae8e065f5d7db80331fc43cef882dd8d
yuyang-sun-2019 <81420946+yuyang-sun-2019@users.noreply.github.com>
2023-04-02 12:35:46 +0800

    Update general_sentiments.php

--- salesForecasting/general_sentiments.php
+++ salesForecasting/general_sentiments.php
@@ -5,5 +5,5 @@
 <script type="module" src="https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js"></script>
 
 <tableau-viz id="tableauViz"       
-  src='https://public.tableau.com/app/profile/yuyang.sun/viz/General_HWZ/Dashboard?publish=yes'>
+  src='https://public.tableau.com/views/General_HWZ/Dashboard?:language=en-GB&publish=yes&:display_count=n&:origin=viz_share_link'>
 </tableau-viz>
