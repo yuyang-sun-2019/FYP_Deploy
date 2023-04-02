0df328183ce60bde4bdac616da11aa81150a39e2
yuyang-sun-2019 <81420946+yuyang-sun-2019@users.noreply.github.com>
2023-04-02 12:25:26 +0800

    visuals

---
+++ salesForecasting/general_sentiments.php
@@ -0,0 +1,5 @@
+<script type="module" src="https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js"></script>
+
+<tableau-viz id="tableauViz"       
+  src='https://public.tableau.com/app/profile/yuyang.sun/viz/General_HWZ/Dashboard?publish=yes'>
+</tableau-viz>
